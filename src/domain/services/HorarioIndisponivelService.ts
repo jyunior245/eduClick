f39235e3b67b2cb75b504e1220bf135c0702faf7
vanessapereiracunha <vanessapereiracunha72@gmail.com>
2025-07-08 21:00:01 -0300

    Implementaçãdo serviço de Horário indisponível

---
+++ src/domain/services/HorarioIndisponivelService.ts
@@ -0,0 +1,19 @@
+import { HorarioIndisponivel } from "../../core/entities/HorarioIndisponivel";
+import { IRepository } from "../../core/interfaces/IRepository";
+
+export class HorarioIndisponivelService {
+  constructor(private horarioRepository: IRepository<HorarioIndisponivel>) {}
+
+  async criar(horario: HorarioIndisponivel): Promise<void> {
+    await this.horarioRepository.salvar(horario);
+  }
+
+  async listarPorProfessor(professorId: string): Promise<HorarioIndisponivel[]> {
+    const todos = await this.horarioRepository.listarTodos();
+    return todos.filter(h => h.professorId === professorId);
+  }
+
+  async remover(id: string): Promise<void> {
+    await this.horarioRepository.remover(id);
+  }
+} 
