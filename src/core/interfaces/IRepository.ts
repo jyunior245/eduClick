bbb913456e253a264ac713d9bd2b787e9fe81d7b
Jyunior <francisniltodossantos@gmail.com>
2025-08-11 20:10:08 -0300

    Removendo arquivos fora de educlick-nova-interface

--- src/core/interfaces/IRepository.ts
+++
@@ -1,6 +0,0 @@
-export interface IRepository<T> {
-  salvar(entidade: T): Promise<void>;
-  buscarPorId(id: string): Promise<T | null>;
-  listarTodos(): Promise<T[]>;
-  remover(id: string): Promise<void>;
-}
--- src/tests/domain/UsuarioService.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from '@jest/globals';
-
-describe('Teste de Sanidade', () => {
-  it('1 + 1 deve ser igual a 2', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
